f1191919f56373e8b6379df1c3acff2bfe476e26
hdeza <armandohdeza.03@gmail.com>
2024-04-08 20:47:12 -0500

    Estilos a la app

--- src/components/task/Task.jsx
+++ src/components/task/Task.jsx
@@ -1,11 +1,16 @@
+import { useState } from 'react';
 import './Task.css';
+import { MdOutlineDeleteOutline } from "react-icons/md";
+
 
 export const Task = ({taskText, deleteTask}) =>{
     return(
         <div className='tarea'>
             {/* <input type="checkbox" name="" id="" /> */}
-            <h1>{taskText}</h1>
-            <button onClick={deleteTask}>x</button>
+            <h1>{taskText}</h1> 
+            <button onClick={deleteTask}>
+                <MdOutlineDeleteOutline className='delete'/>
+            </button>
             {/* Al cambiar onClick={deleteTask(index)} a onClick={() => deleteTask(index)}, evitas la invocación de deleteTask 
             directamente durante la renderización del componente Task. En su lugar, pasas una función de callback que se 
             ejecutará cuando se haga clic en el botón. */}
